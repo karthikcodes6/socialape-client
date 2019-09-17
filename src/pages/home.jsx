c03f4bb86317e3f0ba2a6f1eb65e8c82301b53c6
Karthik <karthikeyan@splitchennai.com>
2019-09-17 23:13:39 +0530

    material-ui added and navbar completion

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+          <p>content....</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>content....</p>
+        </Grid>
+      </Grid>
     );
   }
 }
